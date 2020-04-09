ca42098ceeba52e398deed287c6b542573e92af4
Canadiak <40575641+Canadiak@users.noreply.github.com>
2020-04-09 13:11:58 -0400

    More log in work
    Now you have to log in with valid username and password from the databse to access the site.

--- assignment_3/app.py
+++ assignment_3/app.py
@@ -1,23 +1,26 @@
 from flask import Flask, render_template, url_for, redirect, request, g
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_wtf import FlaskForm 
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
 import sqlite3
 DATABASE = './login.db'
 #The stuff below is from the Week 13 tutorial
 # connects to the database
 def get_db():
-    # if there is a database, use it
-    db = getattr(g, '_login', None)
-    if db is None:
-        # otherwise, create a database to use
-        db = g._login = sqlite3.connect(DATABASE)
-    return db
+	# if there is a database, use it
+	db = getattr(g, '_login', None)
+	if db is None:
+	   # otherwise, create a database to use
+	   db = g._login = sqlite3.connect(DATABASE)
+	return db
 
 # converts the tuples from get_db() into dictionaries
 # (don't worry if you don't understand this code)
 def make_dicts(cursor, row):
-    return dict((cursor.description[idx][0], value)
-                for idx, value in enumerate(row))
+	return dict((cursor.description[idx][0], value)
+			 for idx, value in enumerate(row))
 
 # given a SELECT query, executes and returns the result
 # (don't worry if you don't understand this code)
@@ -35,7 +38,7 @@ app = Flask(__name__)
 def close_connection(exception):
 	db = getattr(g, '_database', None)
 	if db is not None:
-        # close the database if we are connected to it
+	   # close the database if we are connected to it
 		db.close()
 
 #It makes the database login.db and initiates the login manager. Source: https://www.youtube.com/watch?v=8aTnmsDMldY
@@ -45,11 +48,21 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+class LoginForm(FlaskForm):
+	username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+	password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+	#remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+	#email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+	username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+	password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
 #User class, it's what we send and get from the database. Source: https://www.youtube.com/watch?v=8aTnmsDMldY
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(15), unique=True)
-    password = db.Column(db.String(80))
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(15), unique=True)
+	password = db.Column(db.String(80))
 	
 @login_manager.user_loader
 def load_user(user_id):
@@ -75,9 +88,40 @@ def login():
 
 @app.route('/submitLogin', methods=['POST'])
 def submitLogin():
-	user = User.query.filter_by(username='Jeremy').first()
-	login_user(user)
-	return redirect(url_for('home'))
+	#user = User.query.filter_by(username='Jeremy').first()
+	db = get_db()
+	db.row_factory = make_dicts
+	cur = db.cursor()
+
+	username = request.form['username']
+	password = request.form['password']
+	cur.close()
+	user = query_db('select * from user where username = ?', [username], one=True)
+	if user:
+		if (password == user['password']):
+			user = User.query.filter_by(username=username).first()
+			login_user(user)
+			return redirect(url_for('home'))
+
+	return render_template('failedLogin.html')
+	
+	form = LoginForm()
+
+	# if form.validate_on_submit():
+		# print("Check1")
+		# user = User.query.filter_by(username=form.username.data).first()
+		# print(type(user))
+		# if user:
+			# if (user.password == form.password.data):
+				# login_user(user)
+				# return redirect(url_for('home'))
+	# print("Check0")
+	# return render_template('failedLogin.html')
+	
+@app.route('/submitRegistration', methods=['POST'])
+def submitRegister():
+	return render_template('failedLogin.html')
+	
 	
 @app.route('/home')
 @login_required
